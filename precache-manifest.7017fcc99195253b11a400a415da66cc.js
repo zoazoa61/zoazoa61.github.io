b5d19cbf3934293b86cbddd52baea48a0595c5dc
zoazoa61 <zoazoa61@gmail.com>
2020-10-16 17:55:56 +0900

    Updates

--- precache-manifest.7017fcc99195253b11a400a415da66cc.js
+++ precache-manifest.7017fcc99195253b11a400a415da66cc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5f09d218a12f676a445a692dfd85d4bf",
+    "revision": "7011947e6720973a9551e8c430f9b3e8",
     "url": "/index.html"
   },
   {
-    "revision": "4edcdd9c66e0a4e8505f",
+    "revision": "1ab0d33227a2dec824a8",
     "url": "/static/css/main.6f34f3b0.chunk.css"
   },
   {
@@ -16,15 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bd2bf9b9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4edcdd9c66e0a4e8505f",
-    "url": "/static/js/main.ad0ee2d5.chunk.js"
+    "revision": "1ab0d33227a2dec824a8",
+    "url": "/static/js/main.8519e0d3.chunk.js"
   },
   {
     "revision": "b263b198b1cc280d2e73",
     "url": "/static/js/runtime-main.52ef3cae.js"
   },
   {
-    "revision": "348d9c17ef01e11403b71b500780e863",
-    "url": "/static/media/profile.348d9c17.png"
+    "revision": "a3735ddda7ed76bfb682b3dcaa900908",
+    "url": "/static/media/profile.a3735ddd.png"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.806f8a77c6af352542d6de81b10d0f2c.js"
+  "/precache-manifest.7017fcc99195253b11a400a415da66cc.js"
 );
 
 self.addEventListener('message', (event) => {
